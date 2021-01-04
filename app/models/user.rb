fb6bcf45f0b25abd61c0fa55476166ae366c5e16
kenjiono <70374471+kenjiono@users.noreply.github.com>
2021-01-04 14:10:48 +0900

    新規登録・ログイン/ログアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  validates :email, inclusion: { in: @ }
+  validates :email, uniqueness: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{6}+\z/
+  validates :password, presence: true, length: { minimum: 6}, format: { with: VALID_PASSWORD_REGEX}
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+    validates :last_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
+  
   validates :birth_date, presence: true
 
 end
